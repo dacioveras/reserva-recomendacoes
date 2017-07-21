03c5fddcb66cba64ebc0897384e6f0f2b4b5f785
jairorudas <jairo.paternina@usereserva.com>
2017-07-21 19:00:51 -0300

    Finalização do componente para mostrar as opções de cores.

--- js/components.js
+++ js/components.js
@@ -1,36 +1,36 @@
 var templateImagem = '<div > \
-            <div>\
-                <options-colors :cores="grupo.produtos[0].produtoCor"></options-colors>\
-                <a :href="grupo.produtos[0].productUrl"> \
-                    <div class="mz-slider--content">\
-                        <div class="mz-slider--content__image">\
-                            <img class="img-product" :src="grupo.produtos[0].produtoImage" onerror="this.style.display=\'none\'"/>\
-                        </div> \
-                        <div class="mz-slider--content__description">\
-                            <div class="mz-slider--content__description--top">\
-                                <h5 >{{grupo.produtos[0].productTitle}}</h5>\
-                            </div>\
-                            <div class="mz-slider--content__description--bottom">\
-                                <span class="mz-atom--price">\
-                                    <span v-if="grupo.produtos[0].productPrice === grupo.produtos[0].productPriceWithOption">\
-                                        <span class="mz-atom--price__value">R$ {{ grupo.produtos[0].productPrice }}.00</span>\
-                                    </span>\
-                                    <span v-else>\
-                                        <span class="mz-atom--price__desc">De R$ {{ grupo.produtos[0].productPriceWithOption }}.00 por</span>\
-                                        <span class="mz-atom--price__value"> R$ {{ grupo.produtos[0].productPrice }}.00</span>\
-                                    </span>\
-                                    <span v-if="(grupo.produtos[0].productPrice / 60) >= 2">\
-                                        <span class="mz-atom--price__desc">\
-                                            <br />{{ ( grupo.produtos[0].productPrice / 60 ).toFixed() }}X de R$ {{ ( (grupo.produtos[0].productPrice) / ( (grupo.produtos[0].productPrice / 60).toFixed() ) ).toFixed(2) }}\
+                <div class="reserva-image">\
+                    <options-colors :cores="grupo.produtos" v-on:emitcor="selectcor"></options-colors>\
+                    <a class="link-loja" :href="grupo.produtos[0].productUrl"> \
+                        <div class="mz-slider--content">\
+                            <div class="mz-slider--content__image">\
+                                <img class="img-product" :src="grupo.produtos[0].produtoImage" onerror="this.style.display=\'none\'"/>\
+                            </div> \
+                            <div class="mz-slider--content__description">\
+                                <div class="mz-slider--content__description--top">\
+                                    <h5 >{{grupo.produtos[0].productTitle}}</h5>\
+                                </div>\
+                                <div class="mz-slider--content__description--bottom">\
+                                    <span class="mz-atom--price">\
+                                        <span v-if="grupo.produtos[0].productPrice === grupo.produtos[0].productPriceWithOption">\
+                                            <span class="mz-atom--price__value">R$ {{ grupo.produtos[0].productPrice }}.00</span>\
                                         </span>\
+                                        <span v-else>\
+                                            <span class="mz-atom--price__desc">De R$ {{ grupo.produtos[0].productPriceWithOption }}.00 por</span>\
+                                            <span class="mz-atom--price__value"> R$ {{ grupo.produtos[0].productPrice }}.00</span>\
+                                        </span>\
+                                        <span v-if="(grupo.produtos[0].productPrice / 60) >= 2">\
+                                            <span class="mz-atom--price__desc">\
+                                                <br />{{ ( grupo.produtos[0].productPrice / 60 ).toFixed() }}X de R$ {{ ( (grupo.produtos[0].productPrice) / ( (grupo.produtos[0].productPrice / 60).toFixed() ) ).toFixed(2) }}\
+                                            </span>\
+                                        </span>\
+                                        <span v-else></span>\
                                     </span>\
-                                    <span v-else></span>\
-                                </span>\
-                            </div>\
-                        </div> \
-                    </div>\
-                </a>\
-            </div>\
+                                </div>\
+                            </div> \
+                        </div>\
+                    </a>\
+                </div>\
         </div>';
 
 Vue.component('reserva-image', {
@@ -41,9 +41,19 @@ Vue.component('reserva-image', {
                 return {}
             }      
         }
-    },    
-    mounted: function() {
-        return  console.log(this.grupo, 'Mounted');
+    },
+    data: function(){
+        productUrl : this.grupo.produtos[0].productUrl
+    },
+    methods:{
+        selectcor: function(item){
+            console.log('------------------------------------');
+            console.log(item);
+            console.log('------------------------------------');
+            var cor = item.produtoCor.split('/')[5].substr(0, 3);
+            $(this.$el).find('.img-product').attr("src", item.produtoImage );
+            $(this.$el).find('.link-loja').attr("href", item.productUrl + '?prodc=' + cor);
+        }
     },
     template: templateImagem
   })
@@ -51,11 +61,11 @@ Vue.component('reserva-image', {
 
   var colors = '<div class="options-colors">\
                     <ul>\
-                        <li class="liqui" v-for="cor in cores.produtoCor">\
+                        <li class="liqui" v-for="cor in cores" v-on:click="emitcor(cor)">\
                             <img :src="cor.produtoCor" alt="cor.productTitle">\
                         </li>\
                     </ul>\
-                    <button> adicionar à sacola </button>\
+                    <button class="hidden"> <a class="link-loja" :href="cores[0].productUrl"> adicionar à sacola </a> </button>\
                 </div>'
 
   //componentes das opções de cores.
@@ -66,6 +76,11 @@ Vue.component('reserva-image', {
             default: function(){
                 return {}
             }
+        }
+    },
+    methods:{
+        emitcor: function(cor){
+            this.$emit('emitcor', cor);
         }
     },
     template: colors
